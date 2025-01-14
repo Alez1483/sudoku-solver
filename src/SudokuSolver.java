f8b9b230294fd141cd9f288507a1e2d2513d849b
Aksiluhi <aksiluh@gmail.com>
2025-01-15 01:30:01 +0200

    start of the solver

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,4 +1,28 @@
 public class SudokuSolver
 {
-    
+    private int[][][] possibilities = new int[9][9][9]; //very wasteful for now
+    private SudokuBoard board;
+
+    public SudokuSolver(SudokuBoard board)
+    {
+        this.board = board;
+
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                int[] digits = possibilities[x][y];
+
+                for (int i = 0; i < 9; i++)
+                {
+                    digits[i] = i + 1;
+                }
+            }
+        }
+    }
+
+    public int[] getDigits(int x, int y)
+    {
+        return possibilities[x][y];
+    }
 }
--- src/SudokuSolverApp.java
+++ src/SudokuSolverApp.java
@@ -1,12 +1,18 @@
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 public class SudokuSolverApp
 {
     JFrame frame;
+    private final int width = 500;
+    private final int height = 500;
+    private final int lineThickness = 1;
     public static void main(String[] args)
     {
         SudokuSolverApp app = new SudokuSolverApp();
@@ -31,12 +37,29 @@ public class SudokuSolverApp
         '-----'-----'-----'
         """;
         SudokuBoard board = new SudokuBoard(sudokuToSolve);
+        SudokuSolver solver = new SudokuSolver(board);
 
         frame = new JFrame("Sudoku Solver");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(500, 500));
-        frame.setResizable(false);
-        Container pane = frame.getContentPane();
+        //frame.setSize(new Dimension(width, height));
+        //frame.setResizable(false);
+        JPanel sudokuArea = new JPanel(null);
+        sudokuArea.setSize(width, height);
+        //frame.setContentPane(sudokuArea);
+        frame.add(sudokuArea);
+        frame.pack();
+        
+
+        for (int x = 0; x < 1; x++)
+        {
+            JPanel rectangle = new JPanel();
+            rectangle.setBackground(Color.black);
+            rectangle.setBorder(BorderFactory.createLineBorder(Color.green, lineThickness));
+            rectangle.setBounds(250, 250, 100, 100);
+
+            frame.add(rectangle);
+        }
+
         frame.setVisible(true);
     }
 }
